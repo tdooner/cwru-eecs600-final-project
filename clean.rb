12e7cb890e4df03431efad05524ce316bfb2c2b1
Tom Dooner <tomdooner@gmail.com>
2011-12-13 23:15:27 -0500

    Commit all the things.

--- clean.rb
+++ clean.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class String
     # Returns substrings that begin with the first character.
     # ==== Examples
@@ -14,11 +16,30 @@ clean << ["Identified"]
 clean = clean.flatten.keep_if{|x| x.length > 3}.sort{|x,y| y.length <=> x.length}
 
 
-f = File.open(ARGV[0]).readlines.to_a
+f = File.open(ARGV[0],"rb").read()
+f = f.encode("UTF-8",{:undef=>:replace,:replace=>" "}).split("\n").to_a
 # Remove all series of extended whitespace
 f = f.map{|x| x.chomp.gsub( /\s{2,}/, "" ) }
+# Remove all extended equal signs, because those don't separate anything meaningful.
+f = f.map{|x| x.gsub( /(=){2,}/, "") }
 # Remove all things in "clean" array.
 f = f.map{|x| clean.inject(x){|x, delete_str| x = x.gsub(delete_str, ""); } }
+# Clean any words which begin with a dash (since it's an artifact of the cleaning)
+f = f.map{|x| x.gsub( /-([a-zA-Z]+)/, '\1' ) }
 # Remove all empty lines
 f = f.keep_if{|x| not x.chomp.empty?}
-puts f.join("||")
+
+# Now let's find the lines that separate the file into sections.
+sections = []
+f = f.map do |x| 
+    if x.match(/^[A-Z ]+$/)
+        sections << [x,[]]
+    else
+        begin
+            sections.last.last << x
+        rescue
+        end
+    end
+end
+
+puts Hash[sections].to_json
